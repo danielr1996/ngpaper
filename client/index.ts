efb74e9e9ac2939a9fe02dd9b3214b113d956b73
Daniel Richter <danielrichter@posteo.de>
2023-03-03 05:52:11 +0100

    refactor: use cliffy for cli args parsing
    Signed-off-by: Daniel Richter <danielrichter@posteo.de>

---
+++ client/index.ts
@@ -0,0 +1,16 @@
+import { Command } from "https://deno.land/x/cliffy@v0.25.7/command/mod.ts";
+import { startTunnel } from "./tunnel.ts";
+await new Command()
+  .name("ngpaper")
+  .version("0.1.3")
+  .description("forward ports on your machine to a public host with ssh tunnels")
+  .arguments("<port> [output:string]")
+  .option('-u, --user','ssh root to establish connection with',{default: 'root'})
+  .option('-H, --host','ssh host to establish connection with',{default: 'ssh.ngpaper.danielrichter.codes'})
+  .option('-P, --port <port:number>','port on the ssh host to expose',{default: 0})
+  .option('-d, --debug <debug:boolean>','port on the ssh host to expose',{default: false})
+  .action(async ({user,host,port,debug},localPort)=>{
+        const assignedPort = await startTunnel({remote:{host,port},local: {host: 'localhost',port: localPort},user,debug});
+        console.log(`📜 started forwarding for 🔗 http://${host}:${assignedPort} => localhost:${localPort}`,);
+    })
+  .parse(Deno.args);
---
+++ client/tunnel.ts
@@ -0,0 +1,34 @@
+import { iterateReader } from "https://deno.land/std@0.178.0/streams/iterate_reader.ts";
+
+
+export type TunnelEndpoint = {host: string, port: number}
+
+export type TunnelArguments = {
+    remote: TunnelEndpoint,
+    local: TunnelEndpoint,
+    user: string,
+    debug: boolean
+}
+
+export const startTunnel = ({ remote, local, user,debug=false }: TunnelArguments) =>
+  new Promise(async (resolve, reject) => {
+    const command =
+      `ssh -o StrictHostKeyChecking=no -NR ${remote.port}:${local.host}:${local.port} ${user}@${remote.host}`;
+      if(debug)console.log(`ℹ️ Using ssh command "${command}"`);
+    const cmd = command.split(" ");
+    const proc = Deno.run({ cmd, stdout: "piped", stderr: "piped" });
+    if (remote.port != 0) resolve(remote.port);
+
+    for await (const chunk of iterateReader(proc.stderr)) {
+      const found = new TextDecoder().decode(chunk).toString().match(
+        /Allocated port ([0-9]+) for remote/,
+      );
+      if (found) {
+        const [, port] = found;
+        if (port) {
+          proc.stdout.close();
+          resolve(port);
+        }
+      }
+    }
+  });
