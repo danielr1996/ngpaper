c1a7a201e0f62ce8175b6a43c313c60a7d9891bb
Daniel Richter <danielrichter@posteo.de>
2023-03-03 14:37:47 +0100

    WIP
    Signed-off-by: Daniel Richter <danielrichter@posteo.de>

--- client/index.ts
+++ client/index.ts
@@ -1,25 +1,33 @@
 import { Command } from "https://deno.land/x/cliffy@v0.25.7/command/mod.ts";
 import { startTunnel } from "./tunnel.ts";
+
 await new Command()
-  .name("ngpaper")
-  .version("0.1.3")
-  .description("forward ports on your machine to a public host with ssh tunnels")
-  .arguments("<port> [output:string]")
-  .option('-u, --user','ssh root to establish connection with',{default: 'root'})
-  .option('-H, --host','ssh host to establish connection with',{default: 'ssh.ngpaper.danielrichter.codes'})
-  .option('-P, --port <port:number>','port on the ssh host to expose',{default: 0})
-  .option('-d, --debug <debug:boolean>','port on the ssh host to expose',{default: false})
-  .action(async ({user,host,port,debug},localPort)=>{
+    .name("ngpaper")
+    .version("0.1.3")
+    .description("forward ports on your machine to a public host with ssh tunnels")
+    .option('-u, --user', 'ssh root to establish connection with', { default: 'root' })
+    .option('-P, --port <port:number>', 'port on the ssh host to expose', { default: 0 })
+    .option('-d, --debug <debug:boolean>', 'port on the ssh host to expose', { default: false })
+    .arguments("<port> [output:string]")
+    .action(async ({ user, port, debug }, host, localPort) => {
         const assignedPort = await startTunnel({
-            remote:{
+            remote: {
                 host: host as string,
-                port: port as number},
+                port: port as number
+            },
             local: {
                 host: 'localhost',
-                port: (localPort as unknown) as number},
+                port: (localPort as unknown) as number
+            },
             user: user as string,
             debug: debug as boolean,
         });
-        console.log(`📜 started forwarding for 🔗 http://${host}:${assignedPort} => localhost:${localPort}`,);
+        const res = (await fetch("http://api.ngpaper.danielrichter.codes/routes", {
+            method: 'POST', body: JSON.stringify({
+                hostname: host, port: assignedPort
+            })
+        })).text()
+        console.log(res)
+        console.log(`📜 started forwarding for 🔗 http://${host} => localhost:${localPort}`,);
     })
-  .parse(Deno.args);
+    .parse(Deno.args);
--- client/tunnel.ts
+++ client/tunnel.ts
@@ -1,20 +1,20 @@
 import { iterateReader } from "https://deno.land/std@0.178.0/streams/iterate_reader.ts";
 
 
-export type TunnelEndpoint = {host: string, port: number}
+export type TunnelEndpoint = { host: string, port: number }
 
 export type TunnelArguments = {
-    remote: TunnelEndpoint,
-    local: TunnelEndpoint,
-    user: string,
-    debug: boolean
+  remote: TunnelEndpoint,
+  local: TunnelEndpoint,
+  user: string,
+  debug: boolean
 }
 
-export const startTunnel = ({ remote, local, user,debug=false }: TunnelArguments) =>
+export const startTunnel = ({ remote, local, user, debug = false }: TunnelArguments) =>
   new Promise(async (resolve, reject) => {
     const command =
       `ssh -o StrictHostKeyChecking=no -NR ${remote.port}:${local.host}:${local.port} ${user}@${remote.host}`;
-      if(debug)console.log(`ℹ️ Using ssh command "${command}"`);
+    if (debug) console.log(`ℹ️ Using ssh command "${command}"`);
     const cmd = command.split(" ");
     const proc = Deno.run({ cmd, stdout: "piped", stderr: "piped" });
     if (remote.port != 0) resolve(remote.port);
